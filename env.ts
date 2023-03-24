e3426239fee68da4b3a429937e043477d394d8c4
Kyle Saburao <ksaburao@sfu.ca>
2023-03-24 09:56:48 -0700

    add project id env var

--- env.ts
+++ env.ts
@@ -6,6 +6,7 @@ const ENV = {
   JWT_SECRET: process.env.JWT_SECRET || "secret",
   GATEWAY_DOMAIN: process.env.GATEWAY_DOMAIN || "http://localhost",
   WEBAPP_DOMAIN: process.env.WEBAPP_DOMAIN || "http://localhost",
+  PROJECT_ID: process.env.PROJECT_ID || DEFAULT,
 };
 
 for (const [k, v] of Object.entries(ENV)) {
