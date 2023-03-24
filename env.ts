418944e0fe25bdbc492f364d9d6854fb604eb5fd
Kyle Saburao <ksaburao@sfu.ca>
2023-03-24 08:33:29 -0700

    fix env for webapp domain

--- env.ts
+++ env.ts
@@ -5,7 +5,7 @@ const DEFAULT = "";
 const ENV = {
   JWT_SECRET: process.env.JWT_SECRET || "secret",
   GATEWAY_DOMAIN: process.env.GATEWAY_DOMAIN || "http://localhost",
-  WEBAPP_DOMAIN: process.env.GATEWAY_DOMAIN || "http://localhost",
+  WEBAPP_DOMAIN: process.env.WEBAPP_DOMAIN || "http://localhost",
 };
 
 for (const [k, v] of Object.entries(ENV)) {
