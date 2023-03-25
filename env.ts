618bbf534eeb4837068d8a0fd26f91e5c906a39c
Kyle Saburao <ksaburao@sfu.ca>
2023-03-25 15:28:45 -0700

    add mentor application password to env

--- env.ts
+++ env.ts
@@ -4,6 +4,8 @@ const DEFAULT = "";
 
 const ENV = {
   JWT_SECRET: process.env.JWT_SECRET || "secret",
+  MENTOR_APPLICATION_PASSWORD:
+    process.env.MENTOR_APPLICATION_PASSWORD || "mentorpls",
   GATEWAY_DOMAIN: process.env.GATEWAY_DOMAIN || "http://localhost",
   WEBAPP_DOMAIN: process.env.WEBAPP_DOMAIN || "http://localhost",
   PROJECT_ID: process.env.PROJECT_ID || DEFAULT,
