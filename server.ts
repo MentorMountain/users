c16ecaee795b52158d8b4c8192b8a86034073c28
Kyle Saburao <ksaburao@sfu.ca>
2023-03-25 18:37:03 -0700

    enable cors

--- server.ts
+++ server.ts
@@ -36,8 +36,7 @@ const corsOptions = {
   },
 };
 
-//   app.use(cors(corsOptions));
-app.use(cors({ origin: "*" }));
+  app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
