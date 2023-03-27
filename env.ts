c746611da01118fad118bedfe984238b1dc73a38
Kyle Saburao <ksaburao@sfu.ca>
2023-03-27 00:04:37 -0700

    add users db collection name env

--- env.ts
+++ env.ts
@@ -9,6 +9,7 @@ const ENV = {
   GATEWAY_DOMAIN: process.env.GATEWAY_DOMAIN || "http://localhost",
   WEBAPP_DOMAIN: process.env.WEBAPP_DOMAIN || "http://localhost",
   PROJECT_ID: process.env.PROJECT_ID || DEFAULT,
+  DB_COLLECTION_NAME: process.env.DB_COLLECTION_NAME || DEFAULT
 };
 
 for (const [k, v] of Object.entries(ENV)) {
