245cc92a42ee50004facd9caa40b23c57eb3b0c2
Kyle Saburao <ksaburao@sfu.ca>
2023-03-31 08:37:27 -0700

    add HCAPTCHA_VERIFY_KEY

--- env.ts
+++ env.ts
@@ -9,7 +9,8 @@ const ENV = {
   GATEWAY_DOMAIN: process.env.GATEWAY_DOMAIN || "http://localhost",
   WEBAPP_DOMAIN: process.env.WEBAPP_DOMAIN || "http://localhost",
   PROJECT_ID: process.env.PROJECT_ID || DEFAULT,
-  DB_COLLECTION_NAME: process.env.DB_COLLECTION_NAME || DEFAULT
+  DB_COLLECTION_NAME: process.env.DB_COLLECTION_NAME || DEFAULT,
+  HCAPTCHA_VERIFY_KEY: process.env.HCAPTCHA_VERIFY_KEY || DEFAULT
 };
 
 for (const [k, v] of Object.entries(ENV)) {
